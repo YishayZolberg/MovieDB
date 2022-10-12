1a54e95e400d1e67c84fab2aefbde1fec17a5799
YishayZolberg <yishayzolberg@gmail.com>
2022-10-12 10:36:45 +0300

    end of day 1

--- yishay.py
+++ yishay.py
@@ -1,14 +1,16 @@
-import os, requests
+import os, requests, pymongo
 from dotenv import load_dotenv
+
 load_dotenv()
 
 api_key=os.getenv('API_KEY')
-#api_url=os.getenv('URL')
-
+client = pymongo.MongoClient("mongodb+srv://yishayzolberg:O4Mkld5Pe4KJFSF9@cluster0.rdxvksb.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
 user=input("Please enter some movie to search: ")
 search_url=f'https://api.themoviedb.org/3/search/movie?query={user}&api_key={api_key}'
 jpg_path=''
-image_base_url=f'https://image.tmdb.org/t/p/w1280{jpg_path}'
+image_base_url=f'https://image.tmdb.org/t/p/original{jpg_path}'
 blabla=requests.get(search_url).json()
 jpg_path=blabla.get('results')[0]['backdrop_path']
 print(image_base_url,jpg_path, sep='')
